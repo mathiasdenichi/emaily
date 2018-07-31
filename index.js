c65b64340e7031f925c84d67cd65dc3a6b17bfc0
mathiasdenichi <mathiasdenichi@gmail.com>
2018-07-31 14:39:46 -0400

    Next Initial

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-
+const express = require('../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/express')
+const passport = require('../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/passport')
+const GoogleStrategy = require('passport-google-oauth20/lib').Strategy
+const keys = require('./config/keys')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({Adam: 'is a gay head'})
+passport.use(
+  new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+}, 
+accessToken => {
+  console.log(accessToken)
 })
+)
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+  scope: ['profile', 'email'],
+})
+)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.listen(PORT) 
