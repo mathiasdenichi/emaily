b5a22fc2181a15c0060d33efff940dcc105d808e
mathiasdenichi <mathiasdenichi@gmail.com>
2018-08-01 13:28:47 -0400

    Refectored Passport and Auth Routes

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const express = require('../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/express')
-const passport = require('../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/passport')
-const GoogleStrategy = require('passport-google-oauth20/lib').Strategy
-const keys = require('./config/keys')
+const express = require('express')
+require('./services/passport')
 const app = express()
 
-passport.use(
-  new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-}, 
-accessToken => {
-  console.log(accessToken)
-})
-)
-
-app.get(
-  '/auth/google', 
-  passport.authenticate('google', {
-  scope: ['profile', 'email'],
-})
-)
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT) 
