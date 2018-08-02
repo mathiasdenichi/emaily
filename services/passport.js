96ecf5057bb7b69df1651b743595a877e6cebaba
mathiasdenichi <mathiasdenichi@gmail.com>
2018-08-02 11:40:40 -0400

    Finished Oauth routing.

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,15 @@ const mongoose = require('mongoose')
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+})
+
 passport.use(
     new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -12,6 +21,16 @@ passport.use(
     callbackURL: '/auth/google/callback',
   }, 
   (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save()
+
+    User.findOne({ googleId: profile.id })
+      .then(existingUser => {
+        if (existingUser){
+          done(null, existingUser)
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user))
+        }
+      }) 
   })
   )
