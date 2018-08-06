483e676ad1e960940e06c3389ccb817700a37afe
mathiasdenichi <mathiasdenichi@gmail.com>
2018-08-06 12:50:33 -0400

    Create Placeholder Client Side

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers'
+
+import App from './components/App'
+
+const store = createStore(() => reducers, {}, applyMiddleware())
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, 
+    document.getElementById('root')
+)
